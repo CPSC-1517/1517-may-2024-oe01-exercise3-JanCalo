0481871a75b9e183b30ad24378efd973e4c2fc74
Jan Calo <jcalo2@nait.ca>
2024-07-04 18:00:19 -0600

    Placing old projects

---
+++ BookSystem/BookSystem/Author.cs
@@ -0,0 +1,100 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BookSystem
+{
+    public class Author
+    {
+        #region Data Members
+        private string _firstName;
+        private string _lastName;
+        private string _contactUrl;
+        private string _residentCity;
+        private string _residentCountry;
+        #endregion
+
+        #region Properties
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                //first name is required
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("FirstName is required.");
+                }
+                _firstName = value.Trim();
+            }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                //last name is required
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("LastName is required.");
+                }
+                _lastName = value.Trim();
+            }
+        }
+        public string ContactUrl
+        {
+            get { return _contactUrl; }
+            set
+            {
+                string pattern = @"^(https?://www\.)?[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)*\.\w{2,}$";
+                if (string.IsNullOrWhiteSpace(value) || !Regex.IsMatch(value, pattern))
+                {
+                    throw new ArgumentException("ContactUrl is required to match acceptable URL pattern");
+                }
+                _contactUrl = value.Trim();
+            }
+        }
+
+        public string ResidentCity
+        {
+            get { return _residentCity; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("ResidentCity is required.");
+                }
+                _residentCity = value.Trim();
+            }
+        }
+        public string ResidentCountry
+        {
+            get { return _residentCountry; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("ResidentCountry is required");
+                }
+                _residentCountry = value.Trim();
+            }
+        }
+        public string AuthorName => $"{LastName}, {FirstName}";
+        #endregion
+        #region Constructor
+  
+        public Author(string firstName, string lastName, string contactUrl, string residentCity, string residentCountry)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            ContactUrl = contactUrl;
+            ResidentCity = residentCity;
+            ResidentCountry = residentCountry;
+        }
+        #endregion
+        #region Methods
+        public override string ToString()
+        {
+            return $"{FirstName},{LastName},{ContactUrl},{ResidentCity},{ResidentCountry}";
+        }
+        #endregion
+    }
+}
---
+++ BookSystem/BookSystem/Review.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BookSystem
+{
+    public class Review
+    {
+        #region Data Members
+        private string _isbn;
+        private string _comment;
+        private Reviewer _reviewer;
+        #endregion
+        #region Properties
+        public string ISBN
+        {
+            get { return _isbn; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("ISBN is required");
+                }
+                _isbn = value.Trim();
+            }
+        }
+        public Reviewer Reviewer
+        {
+            get { return _reviewer; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Reviewer is required.");
+                }
+                _reviewer = value;
+            }
+        }
+        public RatingType Rating { get; set; }
+        public string Comment
+        {
+            get { return _comment; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Comment is required");
+                }
+                _comment = value.Trim();
+            }
+        }
+        #endregion
+        #region Constructor
+        public Review(string isbn, Reviewer reviewer, RatingType rating, string comment)
+        {
+            ISBN = isbn;
+            Reviewer = reviewer;
+            Rating = rating;
+            Comment = comment;
+        }
+        #endregion
+        #region Methods
+        public override string ToString()
+        {
+            return $"{ISBN},{Reviewer},{Rating},{Comment}";
+        }
+        #endregion
+    }
+}
